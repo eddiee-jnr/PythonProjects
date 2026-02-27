0a065e96dd6f7e5a451288b664130182ab579ca2
eddie-dotcom <edmondpaakumahjnr@gmail.com>
2026-02-27 00:59:50 +0000

    feat: add update expense functionality

--- ExpenseTracker - CLI/expenseTracker.py
+++ ExpenseTracker - CLI/expenseTracker.py
@@ -17,6 +17,7 @@ def load_expenses():
         # If the file doesn't exist yet, return an empty list
         return []
 
+    # master expense list
 my_expenses = load_expenses()
 
 # 1. Create a function to add a new expense
@@ -72,17 +73,17 @@ def delete_expense():
         counter += 1
 
 # Ask the user to select an expense to delete by its number
-    choice = input("Enter the number of the expense you want to delete (or 'q' to cancel): ")
-    if choice.lower() == 'q':
+    del_choice = input("\nEnter the number of the expense you want to delete (or 'q' to cancel): ")
+    if del_choice.lower() == 'q':
         print("Deletion cancelled.")
         return
 
-    if choice.isdigit():
-        choice = int(choice) # Convert the string into a real math integer
+    if del_choice.isdigit():
+        del_choice = int(del_choice)
 
         # Make sure the user's input is a valid number corresponding to an expense
-        if 1 <= choice <= len(my_expenses):
-            deleted_item = my_expenses.pop(choice - 1)
+        if 1 <= del_choice <= len(my_expenses):
+            deleted_item = my_expenses.pop(del_choice - 1)
 
             # Save the newly shortened list to the file
             save_expenses(my_expenses)
@@ -95,13 +96,76 @@ def delete_expense():
         print("Invalid input. Please enter a valid number (e.g., 1, 2, 3).")
 
 
+
+# 7. add to function to update an expense (optional)
+def update_expenses():
+    print("\n -- Update an expense --")
+
+    if not my_expenses:
+        print("No expense to update. Go to add expenses to add")
+        return
+
+    counter = 1
+    for expense in my_expenses:
+        print(f"{counter}. Date: {expense['date']} | Amount: ${expense['price']:.2f} | Category: {expense['category']} | Description: {expense['description']}")
+        counter += 1
+
+    # 7.1 ask user to select an expense to update
+    up_choice = input("\nEnter the number oh the expense you want to update (or 'q' to cancel): ")
+    if up_choice.lower() == 'q':
+        print ("Update Cancelled")
+        return
+
+    if up_choice.isdigit():
+        up_choice = int(up_choice)
+
+        # Make sure the user's input is a valid number corresponding to an expense
+        if 1 <= up_choice <= len(my_expenses):
+            index = up_choice - 1
+
+            # 7.1 Grab the specific dictionary you want to edit
+            target_expense = my_expenses[index]
+            print(f"\nEditing: '{target_expense['description']}'")
+
+            # Ask for the new details (using your exact add_expense logic)
+            new_date = input("Enter the date (e.g., YYYY-MM-DD): ")
+            new_price = input("Enter the price: ")
+        # Validate the price input to ensure it's a number
+            while True:
+                try:
+                    new_price = float(new_price)
+                    break
+                except ValueError:
+                    print("Invalid input for price. Please enter a number (e.g., 10.00)")
+                    new_price = input("Enter the price: ")
+            new_category = input("Enter the category (e.g., Food, Transport): ")
+            new_description = input("Enter a quick description: ")
+
+            # 7.2. Swap the old dictionary values with the new ones
+            target_expense["date"] = new_date
+            target_expense["price"] = new_price
+            target_expense["category"] = new_category
+            target_expense["description"] = new_description
+
+            #7.3 Save the updated list into the file
+            save_expenses(my_expenses)
+            print("Expense Successfully updated!")
+
+        else:
+            print(f"Invalid number. Please choose a number between 1 and {len(my_expenses)}.")
+    else:
+        print("Invalid input. Please enter a valid number (e.g., 1, 2, 3).")
+
+
+
 # 3. add a while that acts as a menu to call the functions we have created
 while True:
     print("\n--- Expense Tracker Menu ---")
     print("1. Add a New Expense")
     print("2. View Expenses")
     print("3. Delete an Expense")
-    print("4. Exit")
+    print("4. Update an Expense")
+    print("5. Exit")
 
     choice = input("Please enter an option below: ")
 
@@ -113,7 +177,9 @@ while True:
     elif choice == '3':
         delete_expense()
     elif choice == '4':
-        print("Exiting the program. Goodbye!")
+        update_expenses()
+    elif choice == '5':
+        print("Always keep tarck of your expenses. Goodbye!")
         break
     else:
         print("Invalid choice. Please try again.")
